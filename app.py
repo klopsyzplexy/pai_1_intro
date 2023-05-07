6d39f12c48dfe99588be5369ee1ea3c89d14bc5a
klopsyzplexy <114867350+klopsyzplexy@users.noreply.github.com>
2023-05-07 12:00:22 +0200

    Zadanie2_POST

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import requests
 
-def pobierz_kod():
-	return requests.get("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json").json()
+url = "https://httpbin.org/post"
+head = {"Content-Type": "application/json"}
+data = {"name":"Bogdan"}
 
-def pobierz_dane():
-	if pobierz_kod() == 200:
-		return requests.get("https://mdn.github.io/learnin-area/javascript/oojs/json/superheroes.json").json()
-	else:
-		return None
+
+def post():
+    return requests.post(url, json=data, headers = head).json()
 
 if __name__ == "__main__":
-	print(pobierz_dane())
+    print(post())
